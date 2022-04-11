160dcdc79d97512c0339867bd84af0ce0ae7d81d
Carla <102888606+24Carla@users.noreply.github.com>
2022-04-10 22:00:03 -0300

    Add files via upload

---
+++ proyectoCarla/Animal.py
@@ -0,0 +1,11 @@
+class Animal():
+    def __init__(self,nombre ,peso ,edad):
+       self.nombre = nombre
+       self .peso = peso 
+       self .edad = edad
+    def __init__(self, cant_alimento):
+        self.peso+= cant_alimento
+    def __str__(self) ->str:
+        return' Soy una persona '
+    def __init__(self) ->str:
+        return'perro'    
---
+++ proyectoCarla/Vehiculo.py
@@ -0,0 +1,4 @@
+class Vehiculo():
+    def __init__(self,marca , precio ):
+        self.marca=marca
+        self.precio=precio
