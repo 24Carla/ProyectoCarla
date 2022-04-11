170fe5b925c9030d918d0983552e317d8831aa16
Carla <102888606+24Carla@users.noreply.github.com>
2022-04-10 22:33:06 -0300

    Add files via upload

---
+++ proyectoCarla/Datos .py
@@ -0,0 +1,5 @@
+class Persona():
+    def __init__(self, nombre , edad ,pais):
+        self.nombre=nombre
+        self.edad=edad
+        self.pais=pais
---
+++ proyectoCarla/Familiar.py
@@ -0,0 +1,6 @@
+class Familiar():
+    def __init__(self,nombre ,edad,parentesco):
+       self.nombre = nombre
+       self .edad = edad
+       parentesco=parentesco
+    
